79439f3812a5ce0bb471f087921a614bf8988c75
Cyclopzpp <ebareapersonal@gmail.com>
2025-12-17 22:27:37 -0300

    Update PLAYERS.sql

--- PLAYERS.sql
+++ PLAYERS.sql
@@ -1,8 +1,36 @@
 CREATE TABLE PLAYERS (
 
-    player_name VARCHAR(100) PRIMARY KEY NOT NULL,
-    pj_id INT PRIMARY KEY NOT NULL,
+    player_name VARCHAR(100) NOT NULL,
+    pj_id INT NOT NULL,
     country_name VARCHAR(30) NOT NULL,
-    pj_name as FOREIGN KEY (player_name, pj_id) REFERENCES PJ(player_name, pj_id)
+    PRIMARY KEY (player_name, pj_id)
 
-)
+);
+
+CREATE TABLE PJ (
+    
+    player_name VARCHAR(100) NOT NULL,
+    pj_id INT NOT NULL,
+    pj_name VARCHAR(50),
+    pj_genre CHAR(1), -- "F" o "M"
+    pj_level INT,
+    pj_experience INT,
+    pj_race VARCHAR(30),
+    pj_class VARCHAR(30),
+    PRIMARY KEY (player_name, pj_id),
+    FOREIGN KEY (player_name, pj_id) REFERENCES PLAYERS(player_name, pj_id)
+
+);
+
+CREATE TABLE SPA (
+
+    player_name VARCHAR(100) NOT NULL,
+    pj_id INT NOT NULL,
+    ability_cant INT NOT NULL,
+    special_ability_name VARCHAR(100),
+    special_ability_description TEXT,
+    PRIMARY KEY (player_name, pj_id, ability_cant),
+    CHECK (ability_cant IN (1, 2)),
+    FOREIGN KEY (player_name, pj_id) REFERENCES PLAYERS(player_name, pj_id)
+    
+);
