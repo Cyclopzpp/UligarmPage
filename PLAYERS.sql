0b6a334a875a25c726ba83da2217ae3d92980f2b
Cyclopzpp <ebareapersonal@gmail.com>
2025-12-18 23:03:25 -0300

    Update PLAYERS.sql

--- PLAYERS.sql
+++ PLAYERS.sql
@@ -34,3 +34,36 @@ CREATE TABLE SPA (
     FOREIGN KEY (player_name, pj_id) REFERENCES PLAYERS(player_name, pj_id)
     
 );
+
+CREATE TABLE STATS (
+
+    player_name VARCHAR(100) NOT NULL,
+    pj_id INT NOT NULL,
+    strength INT,
+    strength_mod INT,
+    dexterity INT,
+    dexterity_mod INT,
+    constitution INT,
+    constitution_mod INT,
+    intelligence INT,
+    intelligence_mod INT,
+    wisdom INT,
+    wisdom_mod INT,
+    charisma INT,
+    charisma_mod INT,
+    PRIMARY KEY (player_name, pj_id),
+    CHECK (strength BETWEEN 1 AND 20),
+    CHECK (dexterity BETWEEN 1 AND 20),
+    CHECK (constitution BETWEEN 1 AND 20),
+    CHECK (intelligence BETWEEN 1 AND 20),
+    CHECK (wisdom BETWEEN 1 AND 20),
+    CHECK (charisma BETWEEN 1 AND 20),
+    CHECK (strength_mod = (strength - 10) / 2),
+    CHECK (dexterity_mod = (dexterity - 10) / 2),
+    CHECK (constitution_mod = (constitution - 10) / 2),
+    CHECK (intelligence_mod = (intelligence - 10) / 2),
+    CHECK (wisdom_mod = (wisdom - 10) / 2),
+    CHECK (charisma_mod = (charisma - 10) / 2),
+    FOREIGN KEY (player_name, pj_id) REFERENCES PJ(player_name, pj_id)
+
+);
